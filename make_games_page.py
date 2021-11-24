a7786aad9a395f1c6a94f1dc335f062a7fa57743
tjmurphy <tjmurphy@mit.edu>
2021-11-24 13:56:04 -0500

    Working games list

---
+++ make_games_page.py
@@ -0,0 +1,26 @@
+import csv
+game_array = []
+text_output = ""
+with open("games.csv") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        game_array.append(list(row))
+console_lists = list(zip(*game_array))
+for given_console in console_lists:
+    given_console = list(given_console)
+    while("" in given_console):
+        given_console.remove("")
+    console_name = given_console[0]
+    games = given_console[1:]
+    text_output+=f"<br><h1>{console_name}</h1><p>"
+    text_output+="<br>".join(games) + "</p>"
+
+with open('blankpage.html') as template:
+    pagetext = template.read()
+    header = pagetext.split('<div id="content">')[0] + '<div id="content">\n'
+    header = header.replace("Sample Blank Page","My Video Game Collection")
+    footer = "\n</div>" + pagetext.split("</div>")[-1] + "\n"
+
+with open('games.html','w') as outfile:
+    outfile.write(header + text_output + footer)
+    
