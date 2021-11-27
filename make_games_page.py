0bb7f30763fda1fae91e6c83326c959feaaf6c25
Joey Murphy <tjmurphy@mit.edu>
2021-11-27 11:15:03 -0500

    ingest a few games, added autosorting to the game script so new games just add to bottom of games.csv

--- make_games_page.py
+++ make_games_page.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import zip_longest
 game_array = []
 text_output = ""
 with open("games.csv") as csvfile:
@@ -6,15 +7,18 @@ with open("games.csv") as csvfile:
     for row in reader:
         game_array.append(list(row))
 console_lists = list(zip(*game_array))
-for given_console in console_lists:
-    given_console = list(given_console)
+#yeah yeah this is bad, but i want to be able to replace elements in console_lists
+for i in range(len(console_lists)):
+    given_console = list(console_lists[i])
     while("" in given_console):
         given_console.remove("")
     console_name = given_console[0]
     games = given_console[1:]
+    games = sorted(games)
+    console_lists[i] = ([console_name] + games)
     text_output+=f"\n<h1>{console_name}</h1>\n<p>\n"
     text_output+="<br>\n".join(games) + "\n</p>"
-
+print(console_lists)
 with open('blankpage.html') as template:
     pagetext = template.read()
     header = pagetext.split('<div id="content">')[0] + '<div id="content">\n'
@@ -24,4 +28,8 @@ with open('blankpage.html') as template:
 
 with open('games.html','w') as outfile:
     outfile.write(header + text_output + footer)
-    
+#output a sorted version of the csv: each console's games alphabetized
+with open('games.csv','w') as csv_out:
+    outdata = zip_longest(*console_lists,fillvalue='')
+    w = csv.writer(csv_out)
+    w.writerows(outdata)
